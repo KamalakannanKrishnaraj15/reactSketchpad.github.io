c4c2a011dcb34b4dc3d46dbb9b3783d1ac6c62e6
KamalakannanKrishnaraj15 <kamalakannan.krishnaraj15@gmail.com>
2020-11-27 19:37:51 +0530

    Updates

---
+++ precache-manifest.2416965ad6f4940020a0407503a67d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84eac65c27f0cb16297232d14688fcc",
+    "url": "/reactSketchpad.github.io/index.html"
+  },
+  {
+    "revision": "17e040ca6e9c5f3eb30e",
+    "url": "/reactSketchpad.github.io/static/css/main.6c62d757.chunk.css"
+  },
+  {
+    "revision": "0ec5cb965135d81ad659",
+    "url": "/reactSketchpad.github.io/static/js/2.a4502ebb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactSketchpad.github.io/static/js/2.a4502ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e040ca6e9c5f3eb30e",
+    "url": "/reactSketchpad.github.io/static/js/main.715afb2e.chunk.js"
+  },
+  {
+    "revision": "f921d43ba3694f92b79f",
+    "url": "/reactSketchpad.github.io/static/js/runtime-main.77988474.js"
+  }
+]);
